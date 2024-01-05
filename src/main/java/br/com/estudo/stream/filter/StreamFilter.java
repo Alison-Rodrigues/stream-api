320739c1ff8169afbe7267c12c1927337beefe16
Alison Rodrigues <alison.si1302@gmail.com>
2024-01-05 10:21:06 -0300

    feat: more examples for filters

--- src/main/java/br/com/estudo/stream/filter/StreamFilter.java
+++ src/main/java/br/com/estudo/stream/filter/StreamFilter.java
@@ -12,6 +12,9 @@ public class StreamFilter {
     private List<String> names = Arrays.asList("Alison Rodrigues", "Enzo Rodrigues", "Vithor Rodrigues", "Paulo Rodrigues", "Tatiane Rodrigues",
             "Maria Heloisa Trevizzo", "Maria Julia Trevizzo", "Luiz Henrique Trevizzo", "Milza Trevizzo");
 
+    private List<String> campaigns = Arrays.asList("000C260CID0001OrderCampaign123", "000C260CID0001OrderCampaign124", "000C260CID0001ReloadCampaign125",
+            "000C260CID0002ConfirmationCampaign123", "C260CID0002AutomationCampaign1", "C260CID0002AutomationCampaign2", "C260CID0001AutomationCampaign1");
+
     public void executeFilter() {
 
         List<String> resultRodrigues = names
@@ -24,14 +27,38 @@ public class StreamFilter {
                 .filter(s -> s.contains("Trevizzo"))
                 .collect(Collectors.toList());
 
+        List<String> without000 = campaigns
+                .stream()
+                .filter(s -> !s.startsWith("000"))
+                .collect(Collectors.toList());
+
+        List<String> with000 = campaigns
+                .stream()
+                .filter(s -> s.startsWith("000"))
+                .collect(Collectors.toList());
+
+        List<String> without000ContainsCID0001 = campaigns
+                .stream()
+                .filter(s -> !s.startsWith("000") && s.contains("CID0001"))
+                .collect(Collectors.toList());
+
         System.out.println("\n########### STREAM FILTER #############");
 
-        System.out.println("Full array: " + names);
+        System.out.println("Full array NAMES: " + names);
 
         System.out.println("########## contains Rodrigues ###########");
         System.out.println(resultRodrigues);
 
         System.out.println("########## contains Trevizzo ###########");
         System.out.println(resultTrevizzo);
+
+        System.out.println("\nFull Array CAMPAIGNS: " + campaigns);
+
+        System.out.println("########## without start with 000 ###########");
+        System.out.println(without000);
+        System.out.println("########## start with 000 ##########");
+        System.out.println(with000);
+        System.out.println("########## without start with 000 and contains CID0001");
+        System.out.println(without000ContainsCID0001);
     }
 }
